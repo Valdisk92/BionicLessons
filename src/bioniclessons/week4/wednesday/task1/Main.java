34f5ba4305f47d2e985b1a1fb712b3ef158c7093
Vladislav Korostel <korostel.v.s@gmail.com>
2014-10-29 10:27:14 +0200

    Week 4, Wednesday lesson, task1

---
+++ src/bioniclessons/week4/wednesday/task1/Main.java
@@ -0,0 +1,10 @@
+package bioniclessons.week4.wednesday.task1;
+
+/**
+ * Created by Владислав on 29.10.2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}

faacf5d0ef63b46b99329869d0aedb9bc3bf6349
Vladislav Korostel <korostel.v.s@gmail.com>
2014-10-31 12:07:10 +0200

    Сериализация и Десериализация

---
+++ src/bioniclessons/week4/friday/task1/Other.java
@@ -0,0 +1,14 @@
+package bioniclessons.week4.friday.task1;
+
+import java.io.Serializable;
+
+/**
+ * Created by Владислав on 31.10.2014.
+ */
+public class Other implements Serializable{
+    int a = 10;
+
+    public Other() {
+        System.out.println("Other Constructor;");
+    }
+}
